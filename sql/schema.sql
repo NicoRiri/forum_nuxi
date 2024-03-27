efcb9c99b566acf87e36cde2f94a46fbd5c1616b
NicoRiri <nicolate.bernardet@gmail.com>
2024-03-27 15:14:17 +0100

    bdd : 🌠 init de la bdd

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,21 +1,44 @@
-CREATE TABLE Forums (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-);
+-- Adminer 4.8.1 MySQL 11.1.2-MariaDB-1:11.1.2+maria~ubu2204 dump
 
-CREATE TABLE Sujets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    forum_id INT,
-    nom VARCHAR(255),
-    message_initial TEXT,
-    FOREIGN KEY (forum_id) REFERENCES Forums(id)
-);
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE TABLE Messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sujet_id INT,
-    timestamp TIMESTAMP,
-    user_id INT,
-    contenu TEXT,
-    FOREIGN KEY (sujet_id) REFERENCES Sujets(id)
-);
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `Forums`;
+CREATE TABLE `Forums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `Messages`;
+CREATE TABLE `Messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sujet_id` int(11) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `contenu` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sujet_id` (`sujet_id`),
+  CONSTRAINT `Messages_ibfk_1` FOREIGN KEY (`sujet_id`) REFERENCES `Sujets` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `Sujets`;
+CREATE TABLE `Sujets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `forum_id` int(11) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `message_initial` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `forum_id` (`forum_id`),
+  CONSTRAINT `Sujets_ibfk_1` FOREIGN KEY (`forum_id`) REFERENCES `Forums` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- 2024-03-27 14:13:16
+

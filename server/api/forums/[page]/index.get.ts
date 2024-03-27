910264b9275ee913a14735cd0157458a454ab969
NicoRiri <nicolate.bernardet@gmail.com>
2024-03-27 16:23:29 +0100

    Get Forum

---
+++ server/api/forums/[page]/index.get.ts
@@ -0,0 +1,11 @@
+import {connection} from "~/server/utils";
+
+export default defineEventHandler(async (event) => {
+    const conn = await connection
+    const page = getRouterParam(event, 'page')
+    const offset = parseInt(page!) * 20
+    const [rows] = await conn.execute("SELECT * FROM Forums LIMIT 20 OFFSET ?", [offset])
+
+    setResponseStatus(event, 200)
+    return rows
+})

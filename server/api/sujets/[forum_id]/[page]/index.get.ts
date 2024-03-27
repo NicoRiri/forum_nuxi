8359cf6c13edc6c8d5165e20d666f93004d8c8c6
NicoRiri <nicolate.bernardet@gmail.com>
2024-03-27 17:36:42 +0100

    correction sql

--- server/api/sujets/[forum_id]/[page]/index.get.ts
+++ server/api/sujets/[forum_id]/[page]/index.get.ts
@@ -2,11 +2,10 @@ import {connection} from "~/server/utils";
 
 export default defineEventHandler(async (event) => {
     const conn = await connection
-
     const page = getRouterParam(event, 'page')
+    const forum_id = getRouterParam(event, 'forum_id')
     const offset = (parseInt(page!)-1) * 20
-    console.log(offset)
-    const [rows] = await conn.execute("SELECT * FROM Forums LIMIT 20 OFFSET ?", [offset])
+    const [rows] = await conn.execute("SELECT * FROM Sujets WHERE forum_id = ? LIMIT 20 OFFSET ?", [forum_id, offset])
 
     setResponseStatus(event, 200)
     return rows

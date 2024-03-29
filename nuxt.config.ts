a7b22a13703185ec5b20df11331d3a57e703706f
NicoRiri <nicolate.bernardet@gmail.com>
2024-03-29 14:35:21 +0100

    import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {transformAssetUrls} from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   build: {

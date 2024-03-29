e83578400331cff84fcc47545efb29a454c4f293
oudin-clement <66120513+oudin-clement@users.noreply.github.com>
2024-03-29 14:19:06 +0100

    Feat/frontend/forum (#6)
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session

---
+++ pages/forums/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Forums</h1>
+  </div>
+</template>
+
+<style scoped>
+
+h1 {
+  font-family: Arial, sans-serif;
+  font-size: 50px;
+  text-align: center;
+  margin-top: 20px;
+}
+
+
+</style>

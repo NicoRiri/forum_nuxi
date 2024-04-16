2bfc17ed8499d63411c7e6f21cd467270e3752db
oudin-clement <66120513+oudin-clement@users.noreply.github.com>
2024-04-16 11:00:23 +0200

    Feat/frontend/forum (#15)
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * import
    
    * Feat/backend/authentification (#10) (#11)
    
    * Feat/frontend/forum (#6)
    
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * ajout du plugin vuetifiy (#7)
    
    * import
    
    * import
    
    * feat: inscription op
    
    * feat: connexion op
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * Feat/backend/main (#12)
    
    * Feat/backend/authentification (#10)
    
    * Feat/frontend/forum (#6)
    
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * ajout du plugin vuetifiy (#7)
    
    * import
    
    * import
    
    * feat: inscription op
    
    * feat: connexion op
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * feat: nbPage
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * Feat/backend/main (#13)
    
    * Feat/backend/authentification (#10)
    
    * Feat/frontend/forum (#6)
    
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * ajout du plugin vuetifiy (#7)
    
    * import
    
    * import
    
    * feat: inscription op
    
    * feat: connexion op
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * feat: nbPage
    
    * refactor: info
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * listage des forums présent dans la bdd
    
    ---------
    
    Co-authored-by: Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
    Co-authored-by: NicoRiri <nicolate.bernardet@gmail.com>

--- pages/forums/index.vue
+++ pages/forums/index.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
 
+const data = ref()
+const page = ref(1)
+const nbpage = ref()
+
+function fetchForums() {
+  $fetch("/api/forums/" + page.value).then((response) => {
+    data.value = response
+  })
+}
+
+function fetchNbPage() {
+  $fetch("/api/forums/nbpage").then((response) => {
+    nbpage.value = response.res
+  })
+}
+
+onMounted(() => {
+  fetchForums()
+  fetchNbPage()
+})
+
 </script>
 
 <template>
   <div>
     <h1>Forums</h1>
+
+    <v-card v-for="d in data"
+            class="text-center mt-8  mx-auto mb-5"
+            border="opacity-50 sm"
+            max-width="360"
+            rounded="xl"
+            variant="text">
+      <router-link :to="'sujet/'+d.id"
+                   class="text-decoration-none text-cyan-darken-1">
+        <v-card-text class="text-h5 font-weight-black">{{ d.name }}</v-card-text>
+      </router-link>
+    </v-card>
+
+    <v-pagination v-model="page" :length="nbpage" @click="fetchForums"></v-pagination>
   </div>
 </template>
 
@@ -15,6 +50,10 @@ h1 {
   font-size: 50px;
   text-align: center;
   margin-top: 20px;
+}
+
+a {
+  width: 25%;
 }
 
 

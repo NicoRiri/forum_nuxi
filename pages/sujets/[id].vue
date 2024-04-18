f5816613f6d94d7e55df1a84fb90b29ac9e98f06
oudin-clement <66120513+oudin-clement@users.noreply.github.com>
2024-04-18 12:20:08 +0200

    Feat/frontend/sujet (#16)
    * import
    
    * Feat/backend/authentification (#10) (#11)
    
    * Feat/frontend/forum (#6)
    
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * ajout du plugin vuetifiy (#7)
    
    * import
    
    * import
    
    * feat: inscription op
    
    * feat: connexion op
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * Feat/backend/main (#12)
    
    * Feat/backend/authentification (#10)
    
    * Feat/frontend/forum (#6)
    
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * ajout du plugin vuetifiy (#7)
    
    * import
    
    * import
    
    * feat: inscription op
    
    * feat: connexion op
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * feat: nbPage
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * Feat/backend/main (#13)
    
    * Feat/backend/authentification (#10)
    
    * Feat/frontend/forum (#6)
    
    * ajout du plugin vuetifiy
    
    * page principale des forums
    
    * connexion, inscription et ajout du module de la session
    
    * ajout du plugin vuetifiy (#7)
    
    * import
    
    * import
    
    * feat: inscription op
    
    * feat: connexion op
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * feat: nbPage
    
    * refactor: info
    
    ---------
    
    Co-authored-by: oudin-clement <66120513+oudin-clement@users.noreply.github.com>
    
    * correction de la route
    
    * page des sujets
    
    * page pour creer des sujets
    
    * navigation vers newSujet
    
    ---------
    
    Co-authored-by: Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
    Co-authored-by: NicoRiri <nicolate.bernardet@gmail.com>

---
+++ pages/sujets/[id].vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+const route = useRoute()
+const idForum = ref(route.params.id)
+const data = ref()
+const page = ref(1)
+const nbpage = ref()
+const error = ref("")
+const {session, refresh, update, reset} = await useSession()
+const connected = ref(false)
+if (session.value!.userid) {
+  connected.value = true
+}
+
+
+function fetchSujet() {
+  $fetch("/api/sujets/" + idForum.value + "/" + page.value).then((response) => {
+    data.value = response
+  })
+}
+
+function toNewSujet() {
+  if (connected.value) {
+    navigateTo("/newSujet/" + idForum)
+  } else {
+    error.value = "Veuillez vous connecter"
+  }
+}
+
+onMounted(() => {
+  fetchSujet()
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Sujets</h1>
+    <router-link to="/forums">
+      <v-btn class="text-cyan-darken-1 ml-3">
+        Retour vers les forums
+      </v-btn>
+    </router-link>
+    <div class="float-right mr-3">
+      <v-btn class="text-deep-orange-darken-2" @click="toNewSujet">
+        Nouveau Sujet
+      </v-btn>
+      <p class="text-red-darken-1 mt-3">
+        {{ error }}
+      </p>
+    </div>
+    <v-card v-for="d in data"
+            class="text-center mt-8  mx-auto mb-5"
+            border="opacity-50 sm"
+            max-width="360"
+            rounded="xl"
+            variant="text">
+      <router-link :to="'message/'+d.id"
+                   class="text-decoration-none text-deep-orange-darken-2">
+        <v-card-text class="text-h5 font-weight-black">
+          {{ d.nom }}
+        </v-card-text>
+      </router-link>
+    </v-card>
+
+    <v-pagination v-show="nbpage > 1" v-model="page" :length="nbpage" @click="fetchSujet"></v-pagination>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-family: Arial, sans-serif;
+  font-size: 50px;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>

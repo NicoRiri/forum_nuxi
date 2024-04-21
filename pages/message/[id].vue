27f9930b6aba940363dc9a7af31803fe788bff4a
oudin-clement <clementoudin3@gmail.com>
2024-04-21 17:48:35 +0200

    la hes

---
+++ pages/message/[id].vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const route = useRoute()
+const idSujet = ref(route.params.id)
+const data = ref()
+const page = ref(1)
+const nbpage = ref()
+const error = ref("")
+const {session, refresh, update, reset} = await useSession()
+const connected = ref(false)
+if (session.value!.userid) {
+  connected.value = true
+}
+
+function fetchMessage() {
+  $fetch("/api/messages/" + idSujet.value + "/" + page.value).then((response) => {
+    data.value = response
+    console.log(response)
+  })
+}
+
+
+onMounted(() => {
+  fetchMessage()
+})
+</script>
+
+<template>
+<div>
+  <h1>Messages</h1>
+  <v-card v-for="d in data"
+          class="text-center mt-8  mx-auto mb-5"
+          border="opacity-50 sm"
+          max-width="360"
+          rounded="xl"
+          variant="text">
+      <v-card-text class="pb-0">
+        Auteur du message :
+        {{ d.auteur }}
+      </v-card-text>
+      <v-card-text>
+        Message :
+        {{ d.contenu }}
+      </v-card-text>
+      <v-card-text class="pt-0">
+        Date du message :
+        {{ d.timestamp.substring(0,10) }}
+      </v-card-text>
+  </v-card>
+  <v-pagination v-show="nbpage > 1" v-model="page" :length="nbpage" @click="fetchMessage"></v-pagination>
+</div>
+</template>
+
+<style scoped>
+
+h1 {
+  font-family: Arial, sans-serif;
+  font-size: 50px;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>

e4b9ee339f474e117a6144921845aeb1cbc89b82
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-26 23:14:17 +0200

    fix: order

--- server/api/sujets/[forum_id]/[page]/index.get.ts
+++ server/api/sujets/[forum_id]/[page]/index.get.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
     const page = getRouterParam(event, 'page')
     const forum_id = getRouterParam(event, 'forum_id')
     const offset = (parseInt(page!)-1) * 20
-    const [rows] = await conn.execute("SELECT Sujets.id, Sujets.nom,forum_id, message_initial, date, Users.nom AS auteur, (SELECT Users.nom FROM Messages INNER JOIN Users ON Messages.user_id = Users.id WHERE Messages.sujet_id = Sujets.id ORDER BY Messages.timestamp DESC LIMIT 1) AS message_der_auteur FROM Sujets INNER JOIN Users ON Sujets.auteur = Users.id WHERE forum_id = ? LIMIT 20 OFFSET ?", [forum_id, offset])
+    const [rows] = await conn.execute("SELECT Sujets.id, Sujets.nom, Sujets.forum_id, Sujets.message_initial, Sujets.date, Users.nom AS auteur, Messages.timestamp AS dernier_message_timestamp FROM Sujets INNER JOIN Users ON Sujets.auteur = Users.id INNER JOIN ( SELECT sujet_id, MAX(timestamp) AS dernier_message_timestamp FROM Messages GROUP BY sujet_id ) AS DerniersMessages ON Sujets.id = DerniersMessages.sujet_id INNER JOIN Messages ON DerniersMessages.sujet_id = Messages.sujet_id AND DerniersMessages.dernier_message_timestamp = Messages.timestamp INNER JOIN Users AS LastMessageUser ON Messages.user_id = LastMessageUser.id WHERE Sujets.forum_id = ? ORDER BY dernier_message_timestamp DESC LIMIT 20 OFFSET ?", [forum_id, offset])
     setResponseStatus(event, 200)
     return rows
 })

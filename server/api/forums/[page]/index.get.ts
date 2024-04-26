8b47843826e96f52901f2f1f0b880ac573df05b8
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-26 21:52:10 +0200

    feat: forum sujetNb

--- server/api/forums/[page]/index.get.ts
+++ server/api/forums/[page]/index.get.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
 
     const page = getRouterParam(event, 'page')
     const offset = (parseInt(page!)-1) * 20
-    const [rows] = await conn.execute("SELECT * FROM Forums LIMIT 20 OFFSET ?", [offset])
+    const [rows] = await conn.execute("SELECT f.id, f.name, COUNT(s.id) AS sujetNb FROM Forums f LEFT JOIN Sujets s ON f.id = s.forum_id GROUP BY f.id, f.name LIMIT 20 OFFSET ?", [offset])
 
     setResponseStatus(event, 200)
     return rows
